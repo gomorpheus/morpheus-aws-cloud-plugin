c0ea07480c8f05fa1bdceff34dad59b6d697507f
Dan DeVilbiss <ddevilbiss@bcap.com>
2023-07-17 16:44:50 -0700

    vpn gateway sync

--- src/main/groovy/com/morpheusdata/aws/AWSCloudProvider.groovy
+++ src/main/groovy/com/morpheusdata/aws/AWSCloudProvider.groovy
@@ -26,6 +26,7 @@ import com.morpheusdata.aws.sync.VPCSync
 import com.morpheusdata.aws.sync.VirtualMachineSync
 import com.morpheusdata.aws.sync.VolumeSync
 import com.morpheusdata.aws.sync.VpcPeeringConnectionSync
+import com.morpheusdata.aws.sync.VpnGatewaysSync
 import com.morpheusdata.aws.utils.AmazonComputeUtility
 import com.morpheusdata.core.backup.AbstractBackupProvider
 import com.morpheusdata.core.CloudProvider
@@ -37,7 +38,6 @@ import com.morpheusdata.model.Cloud
 import com.morpheusdata.model.ComputeServer
 import com.morpheusdata.model.ComputeServerType
 import com.morpheusdata.model.Icon
-import com.morpheusdata.model.KeyPair
 import com.morpheusdata.model.NetworkProxy
 import com.morpheusdata.model.NetworkSubnetType
 import com.morpheusdata.model.NetworkType
@@ -474,6 +474,7 @@ class AWSCloudProvider implements CloudProvider {
 						new SecurityGroupSync(this.plugin, cloudInfo).execute()
 						new InstanceProfileSync(this.plugin,cloudInfo).execute()
 						new IAMRoleSync(this.plugin,cloudInfo).execute()
+						new VpnGatewaysSync(this.plugin,cloudInfo).execute()
 						new InternetGatewaySync(this.plugin,cloudInfo).execute()
 						//lb services
 						new AlbSync(this.plugin,cloudInfo).execute()
---
+++ src/main/groovy/com/morpheusdata/aws/sync/VpnGatewaySync.groovy
@@ -0,0 +1,96 @@
+package com.morpheusdata.aws.sync
+
+import com.amazonaws.services.ec2.model.VpnGateway
+import com.morpheusdata.aws.AWSPlugin
+import com.morpheusdata.aws.utils.AmazonComputeUtility
+import com.morpheusdata.core.util.SyncTask
+import com.morpheusdata.model.AccountResource
+import com.morpheusdata.model.AccountResourceType
+import com.morpheusdata.model.Cloud
+import com.morpheusdata.model.ComputeZoneRegion
+import com.morpheusdata.model.projection.AccountResourceIdentityProjection
+import com.morpheusdata.model.projection.ComputeZoneRegionIdentityProjection
+import groovy.util.logging.Slf4j
+import io.reactivex.Observable
+import io.reactivex.Single
+
+@Slf4j
+class VpnGatewaysSync extends InternalResourceSync {
+	VpnGatewaysSync(AWSPlugin plugin, Cloud cloud) {
+		this.plugin = plugin
+		this.cloud = cloud
+		this.morpheusContext = plugin.morpheusContext
+	}
+
+	def execute() {
+		morpheusContext.cloud.region.listIdentityProjections(cloud.id).blockingSubscribe { region ->
+			def amazonClient = AmazonComputeUtility.getAmazonClient(cloud,false, region.externalId)
+			def apiList = AmazonComputeUtility.listVpnGateways([amazonClient: amazonClient],[:])
+			if(apiList.success) {
+				Observable<AccountResourceIdentityProjection> domainRecords = morpheusContext.cloud.resource.listIdentityProjections(cloud.id,null, region.externalId).filter {
+					it.typeCode == 'aws.cloudFormation.ec2.vpnGateway'
+				}
+				SyncTask<AccountResourceIdentityProjection, VpnGateway, AccountResource> syncTask = new SyncTask<>(domainRecords, apiList.vpnGateways as Collection<VpnGateway>)
+				syncTask.addMatchFunction { AccountResourceIdentityProjection existingItem, VpnGateway cloudItem ->
+					existingItem.externalId == cloudItem.vpnGatewayId
+				}.withLoadObjectDetailsFromFinder { List<SyncTask.UpdateItem<AccountResourceIdentityProjection, VpnGateway>> updateItems ->
+					morpheusContext.cloud.resource.listById(updateItems.collect { it.existingItem.id } as List<Long>)
+				}.onAdd { itemsToAdd ->
+					addMissingVpnGateways(itemsToAdd, region)
+				}.onUpdate { List<SyncTask.UpdateItem<AccountResource, VpnGateway>> updateItems ->
+					updateMatchedVpnGateways(updateItems, region)
+				}.onDelete { removeItems ->
+					removeMissingResources(removeItems)
+				}.start()
+			} else {
+				log.error("Error Caching VPC Gateways for Region: {} - {}", region.externalId, apiList.msg)
+				return Single.just(false).toObservable() //ignore invalid region
+			}
+		}
+	}
+
+	protected String getCategory() {
+		return "amazon.ec2.vpn.gateway.${cloud.id}"
+	}
+
+	protected void addMissingVpnGateways(Collection<VpnGateway> addList, ComputeZoneRegionIdentityProjection region) {
+		def adds = []
+		for(VpnGateway cloudItem in addList) {
+			def name = cloudItem.tags?.find{it.key == 'Name'}?.value ?: cloudItem.vpnGatewayId
+			AccountResource add = new AccountResource(
+				owner:cloud.account, category:category, code: category + '.' + cloudItem.vpnGatewayId,
+				externalId:cloudItem.vpnGatewayId, cloudId:cloud.id, type: new AccountResourceType(code: 'aws.cloudFormation.ec2.vpnGateway'),
+				resourceType:'VPNGateway', cloudName: cloud.name, name: name, displayName: name, region: new ComputeZoneRegion(id: region.id)
+			)
+			add.configMap = [amazonSideAsn: cloudItem.amazonSideAsn, availabilityZone: cloudItem.availabilityZone]
+			adds << add
+		}
+		morpheusContext.cloud.resource.create(adds).blockingGet()
+	}
+
+	protected void updateMatchedVpnGateways(List<SyncTask.UpdateItem<AccountResource, VpnGateway>> updateList, ComputeZoneRegionIdentityProjection region) {
+		def updates = []
+		for(update in updateList) {
+			def masterItem = update.masterItem
+			def existingItem = update.existingItem
+			Boolean save = false
+			def name = masterItem.tags?.find{it.key == 'Name'}?.value ?: masterItem.vpnGatewayId
+
+			if(existingItem.name != name) {
+				existingItem.name = name
+				save = true
+			}
+			if(existingItem.region?.id != region.id) {
+				existingItem.region = new ComputeZoneRegion(id: region.id)
+				save = true
+			}
+			if(save) {
+				updates << existingItem
+			}
+		}
+		if(updates) {
+			morpheusContext.cloud.resource.save(updates).blockingGet()
+		}
+	}
+
+}
