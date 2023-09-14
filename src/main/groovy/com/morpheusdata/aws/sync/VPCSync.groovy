bb11b8e43e6f1eccf1052f61695610b26053debf
Dan DeVilbiss <ddevilbiss@bcap.com>
2023-09-14 11:36:15 -0700

    fixed vpc name

--- src/main/groovy/com/morpheusdata/aws/sync/VPCSync.groovy
+++ src/main/groovy/com/morpheusdata/aws/sync/VPCSync.groovy
@@ -60,7 +60,7 @@ class VPCSync {
 			def tags = cloudItem.getTags()
 			def nameTag = tags.find{ it.getKey() == 'Name' }
 			def name = nameTag?.value ?: cloudItem.getVpcId()
-			def poolConfig = [owner:[id:cloud.owner.id], type:'vpc', name: "${name} (${region})", description:"${name} - ${cloudItem.getVpcId()} - ${cloudItem.getCidrBlock()}",
+			def poolConfig = [owner:[id:cloud.owner.id], type:'vpc', name:name, displayName:"${name} (${region})", description:"${name} - ${cloudItem.getVpcId()} - ${cloudItem.getCidrBlock()}",
 							  externalId:cloudItem.getVpcId(), refType:'ComputeZone', regionCode: region, refId:cloud.id, cloud:[id:cloud.id], category:"aws.vpc.${cloud.id}",
 							  code:"aws.vpc.${cloud.id}.${cloudItem.getVpcId()}"]
 			def add = new CloudPool(poolConfig)
@@ -84,7 +84,7 @@ class VPCSync {
 
 			def tags = masterItem.getTags()
 			def nameTag = tags.find { it.getKey() == 'Name' }
-			def name = "${nameTag?.value ?: masterItem.getVpcId()} (${region})"
+			def name = nameTag?.value ?: masterItem.getVpcId()
 			def cidr = masterItem.getCidrBlock()
 			def tenancy = masterItem.getInstanceTenancy()
 			def description = "${name} - ${masterItem.getVpcId()} - ${masterItem.getCidrBlock()}"
