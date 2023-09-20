8122c3bfb52a3d75d9b561c48a7cabc98606d122
Dan DeVilbiss <ddevilbiss@bcap.com>
2023-09-20 13:54:06 -0700

    bulk save on vm sync

--- src/main/groovy/com/morpheusdata/aws/sync/VirtualMachineSync.groovy
+++ src/main/groovy/com/morpheusdata/aws/sync/VirtualMachineSync.groovy
@@ -5,6 +5,7 @@ import com.amazonaws.services.ec2.model.InstanceBlockDeviceMapping
 import com.amazonaws.services.ec2.model.Volume
 import com.morpheusdata.aws.AWSPlugin
 import com.morpheusdata.aws.utils.AmazonComputeUtility
+import com.morpheusdata.core.BulkSaveResult
 import com.morpheusdata.core.MorpheusContext
 import com.morpheusdata.core.data.DataQuery
 import com.morpheusdata.core.util.ComputeUtility
@@ -182,45 +183,44 @@ class VirtualMachineSync {
 
 	def updateMatchedVirtualMachines(List<SyncTask.UpdateItem<ComputeServer, Instance>> updateList, CloudRegionIdentity region, Map<String, Volume> volumeMap, Map usageLists, String inventoryLevel) {
 		def statsData = []
-		def recordsToSave = []
-		def managedServerIds = updateList.findAll { it.existingItem.computeServerType?.managed }.collect{it.existingItem.id}
-		def workloads = managedServerIds ? morpheusContext.async.cloud.listCloudWorkloadProjections(cloud.id).filter { workload ->
-			workload.serverId in managedServerIds
-		}.toMap {it.serverId}.blockingGet() : [:]
+		def managedServerIds = updateList.findAll { it.existingItem.computeServerType?.managed && it.existingItem.status != 'provisioning' }.findAll { it.existingItem.computeServerType?.managed }.collect{it.existingItem.id}
+		def workloads = managedServerIds ? morpheusContext.async.workload.list(
+			new DataQuery().withFilter('server.id', 'in', managedServerIds)
+		).toMap {it.serverId}.blockingGet() : [:]
+		List<ComputeServer> saves = []
 
 		for(update in updateList) {
 			ComputeServer currentServer = update.existingItem
 			Instance cloudItem = update.masterItem
 
-			if(currentServer.status != 'provisioning') {
+			if (currentServer.status != 'provisioning') {
 				try {
 					def save = false
 					def name = cloudItem.tags?.find { it.key == 'Name' }?.value ?: cloudItem.instanceId
 					def zonePool = allZonePools[cloudItem.vpcId]
 					def powerState = cloudItem.state?.code == 16 ? ComputeServer.PowerState.on : ComputeServer.PowerState.off
 
-					if(!currentServer.computeServerType) {
+					if (!currentServer.computeServerType) {
 						currentServer.computeServerType = allComputeServerTypes['amazonUnmanaged']
 						save = true
 					}
-					if(currentServer.region?.externalId != region.externalId) {
+					if (currentServer.region?.externalId != region.externalId) {
 						currentServer.region = new CloudRegion(id: region.id)
 						save = true
 					}
-					if(name != currentServer.name) {
+					if (name != currentServer.name) {
 						currentServer.name = name
 						save = true
 					}
-					if(currentServer.resourcePool?.id != zonePool?.id) {
-						currentServer.resourcePool = zonePool?.id ? new CloudPool(id:zonePool.id) : null
+					if (currentServer.resourcePool?.id != zonePool?.id) {
+						currentServer.resourcePool = zonePool?.id ? new CloudPool(id: zonePool.id) : null
 						save = true
 					}
-
-					if(currentServer.externalIp != cloudItem.publicIpAddress) {
-						if(currentServer.externalIp == currentServer.sshHost) {
-							if(cloudItem.publicIpAddress) {
+					if (currentServer.externalIp != cloudItem.publicIpAddress) {
+						if (currentServer.externalIp == currentServer.sshHost) {
+							if (cloudItem.publicIpAddress) {
 								currentServer.sshHost = cloudItem.publicIpAddress
-							} else if(powerState == ComputeServer.PowerState.off) {
+							} else if (powerState == ComputeServer.PowerState.off) {
 								currentServer.sshHost = currentServer.internalIp
 							} else {
 								currentServer.sshHost = null
@@ -229,58 +229,51 @@ class VirtualMachineSync {
 						currentServer.externalIp = cloudItem.publicIpAddress
 						save = true
 					}
-					if(currentServer.internalIp != cloudItem.privateIpAddress) {
-						if(currentServer.internalIp == currentServer.sshHost) {
+					if (currentServer.internalIp != cloudItem.privateIpAddress) {
+						if (currentServer.internalIp == currentServer.sshHost) {
 							currentServer.sshHost = cloudItem.privateIpAddress
 						}
 						currentServer.internalIp = cloudItem.privateIpAddress
 						save = true
 					}
-
-					if(powerState != currentServer.powerState) {
+					if (powerState != currentServer.powerState) {
 						currentServer.powerState = powerState
-						if (currentServer.computeServerType?.guestVm) {
-							morpheusContext.async.computeServer.updatePowerState(currentServer.id, currentServer.powerState).blockingGet()
-						}
-					}
-
-					if (save) {
-						currentServer = saveAndGet(currentServer)
-						save = false
-					}
-
-					def postSaveResults = performPostSaveSync(cloudItem, currentServer, volumeMap)
-
-					if(postSaveResults.planChanged) {
-						if(currentServer.computeServerType?.guestVm) {
-							updateServerContainersAndInstances(currentServer, currentServer.plan)
-						}
-					}
-
-					//check for restart usage records
-					if(postSaveResults.saveRequired) {
-						if (!usageLists.stopUsageIds.contains(currentServer.id) && !usageLists.startUsageIds.contains(currentServer.id)) {
-							usageLists.restartUsageIds << currentServer.id
-						}
 						save = true
 					}
-
 					if (inventoryLevel == 'full' && currentServer.status != 'provisioning' &&
 						(currentServer.agentInstalled == false || currentServer.powerState == ComputeServer.PowerState.off || currentServer.powerState == ComputeServer.PowerState.paused)) {
 						statsData += updateVirtualMachineStats(currentServer, workloads)
 						save = true
 					}
-
 					if (save) {
-						recordsToSave << currentServer
+						saves << currentServer
 					}
 				} catch(e) {
 					log.warn("Error Updating Virtual Machine ${currentServer?.name} - ${currentServer.externalId} - ${e}", e)
 				}
 			}
 		}
-		if(recordsToSave) {
-			morpheusContext.async.computeServer.bulkSave(recordsToSave).blockingGet()
+
+		if(saves) {
+			BulkSaveResult<ComputeServer> saveResult = morpheusContext.async.computeServer.bulkSave(saves).blockingGet()
+			saves = []
+
+			for(ComputeServer currentServer : saveResult.persistedItems) {
+				Instance cloudItem = updateList.find { it.existingItem.id == currentServer.id }.masterItem
+				def postSaveResults = performPostSaveSync(cloudItem, currentServer, volumeMap)
+
+				//check for restart usage records
+				if(postSaveResults.saveRequired) {
+					if (!usageLists.stopUsageIds.contains(currentServer.id) && !usageLists.startUsageIds.contains(currentServer.id)) {
+						usageLists.restartUsageIds << currentServer.id
+					}
+					saves << currentServer
+				}
+			}
+		}
+
+		if(saves) {
+			morpheusContext.async.computeServer.bulkSave(saves).blockingGet()
 		}
 		if(statsData) {
 			for(statData in statsData) {
